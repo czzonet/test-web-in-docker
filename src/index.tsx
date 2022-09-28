34ab70e819535e4387f9956a7428a24f8b91ad21
czzonet <czzonet@163.com>
2022-09-28 09:23:46 +0800

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import App from "./App";
+import "./css/global.css";
+import "./css/reset.css";
+
+ReactDom.render(<App></App>, document.getElementById("app"));
